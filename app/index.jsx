5de1c9fb4ae055d172300fb9b68c9e1468012fcb
ElioFezollari <fezollarielio@gmail.com>
2025-02-13 00:35:10 -0500

    Added Index Page

--- app/index.jsx
+++ app/index.jsx
@@ -1,31 +1,30 @@
-import { useFonts } from 'expo-font'; // Import the useFonts hook
-import { Link } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
+import { useFonts } from 'expo-font'; 
 import { View, Text, StyleSheet, Image } from "react-native";
 import "react-native-reanimated";
 import logo from "../assets/images/index/logo.png";
+import { Link } from 'expo-router';
 
 export default function Index() {
-  // Load the font
   const [fontsLoaded] = useFonts({
-    'Jersey20': require('../assets/fonts/Jersey20.ttf'), // Make sure this path is correct
+    'Jersey20': require('../assets/fonts/Jersey20.ttf'),
   });
 
-  // If fonts aren't loaded yet, show a loading screen (or splash screen)
   if (!fontsLoaded) {
-    return null; // Or you could return a loading spinner, or a splash screen.
+    return null; 
   }
 
   return (
     <View style={styles.container}>
       <View style={styles.viewOne}>
         <Image style={styles.logo} source={logo}/>
-        <View style={styles.viewOne}>
+        <View >
           <Text style={styles.headerText}>Sport Trackr</Text>
         </View>
       </View>
-      <View style={styles.viewTwo}></View>
+      <View style={styles.viewTwo}>
+        <Link style={styles.link} href="/login">Log In</Link>
+        <Link style={{...styles.link,marginTop:20}} href="/register">Sign Up</Link>
+      </View>
     </View>
   );
 }
@@ -43,13 +42,33 @@ const styles = StyleSheet.create({
   },
   viewOne: {
     marginTop: 20,
+    height:"50%",
+    alignContent:"center",
+    justifyContent:"center"
   },
   headerText: {
     textAlign: 'center',
-    fontFamily: 'Jersey20', // Ensure the font name matches the one used when loading the font
-    fontSize: 30
+    fontFamily: 'Jersey20', 
+    fontSize: 40,
+    color:"#495464",
+    marginTop:20,
   },
   viewTwo: {
-    backgroundColor: "green",
+    backgroundColor: "#495464",
+    height:'50%',
+    width:'100%',
+    justifyContent:"center",
+    alignItems:"center",
+    borderTopLeftRadius:60,
+    borderTopRightRadius:60
   },
+  link:{
+    padding:30,
+    fontSize:30,
+    textAlign:'center',
+    backgroundColor:"#E8E8E8",
+    fontFamily: 'Jersey20', 
+    borderRadius:20,
+    width:300
+  }
 });
